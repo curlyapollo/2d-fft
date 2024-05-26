74b9b3cb6cee623345e4f592e968a1fe05f2ecee
Leonid Karlinskiy <95444064+curlyapollo@users.noreply.github.com>
2024-05-26 18:42:16 +0200

    Create wrapping.py

---
+++ wrapping.py
@@ -0,0 +1,37 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QFileDialog, QVBoxLayout, QWidget
+import sys
+
+class MainWindow(QMainWindow):
+    def init(self, parent=None):
+        super(MainWindow, self).init(parent)
+        self.setWindowTitle('2D FFT GUI')
+
+        # Создаем кнопку для загрузки файла
+        self.load_file_button = QPushButton('Load File')
+        self.load_file_button.clicked.connect(self.load_file)
+
+        # Создаем место для отображения имени загруженного файла
+        self.file_label = QLabel()
+
+        # Размещаем кнопку и метку вертикально
+        layout = QVBoxLayout()
+        layout.addWidget(self.load_file_button)
+        layout.addWidget(self.file_label)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+    def load_file(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly
+        file_name, _ = QFileDialog.getOpenFileName(self,'QFileDialog.getOpenFileName()', '','All Files (*);;Python Files (*.py)', options=options)
+        if file_name:
+            self.file_label.setText(file_name)
+            # здесь вы можете добавить код для чтения и обработки файла
+
+if name == 'main':
+    app = QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
